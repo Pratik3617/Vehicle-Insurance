73fb7844d63bf5d6fdd61a65a9dafd2323f93522
Prateek <prateek1736@gmail.com>
2025-06-19 16:02:38 +0000

    data ingestion added

--- demo.py
+++ demo.py
@@ -1,14 +1,28 @@
-from src.logger import logging
+# from src.logger import logging
 
-logging.debug("This is a debug message")
+# logging.debug("This is a debug message")
 
-# below code is to check the exception config
-from src.logger import logging
-from src.exception import MyException
-import sys
+# # below code is to check the exception config
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # This loads variables from .env
+
+mongodb_url = os.getenv("MONGODB_URL")
+
+print("MONGODB_URL from .env:", mongodb_url)
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipeline = TrainPipeline()
+pipeline.run_pipeline()
